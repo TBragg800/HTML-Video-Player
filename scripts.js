e4824ae27871addc9b028b9d229663e20a98573f
LM&T <tbbragg83@gmail.com>
2020-12-28 01:53:26 -0600

    added functions for skipping, volume&speed and progress bar update with event listeners/ fixed incorrect progressBar variable selector

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
 const player = document.querySelector(".player");
 const video = player.querySelector(".viewer");
 const progress = player.querySelector(".progress");
-const progressBar = player.querySelector(".progress_filled");
+const progressBar = player.querySelector(".progress__filled");
 const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
@@ -27,9 +27,27 @@ function updateButton() {
     toggle.textContent = icon;
 }
 
+function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
 //hook up event listeners
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
 
 toggle.addEventListener("click", togglePlay);
+
+skipButtons.forEach(skipButton => skipButton.addEventListener("click", skip));
+
+ranges.forEach(range => range.addEventListener("input", handleRangeUpdate));
